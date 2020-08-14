dd9ba3e9c28bda45349b913b78a3ed1d0d37cdf6
Jamin <jaminstratford@gmail.com>
2020-08-15 10:21:38 +1200

    Update location request

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
+from flask import Flask
 import pymysql
 
 try:
     import config
 except Exception:
-    print ("Please make config file")
+    print("Please make config file")
+
+app = Flask("CoVID-SWARM")
+
+
+@app.route("/location/<int:device_id>", methods=["POST"])
+def update_location(device_id: int):
+    return "Not Implemented"
+
 
 def setupDB():
-    #SQL code goes here
+    # SQL code goes here
