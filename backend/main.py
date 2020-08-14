7a70f3c40b4785717a6a358ccdd586a8f6b5d2f0
Jamin <jaminstratford@gmail.com>
2020-08-15 10:50:52 +1200

    POST Location request

--- backend/main.py
+++ backend/main.py
@@ -1,27 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 import pymysql
 
 try:
     import config
 except Exception:
-    print ("Please make config file and copy contents from dev info file")
+    print("Please make config file and copy contents from dev info file")
 
 app = Flask("CoVID-SWARM")
 
 
-@app.route("/location/<int:device_id>", methods=["POST"])
-def update_location(device_id: int):
-    return "Not Implemented"
+@app.route("/location/<int:device_id>", methods=['POST', 'GET'])
+def location(device_id: int):
+    if request.method == 'POST':
+        # For JSON Payload
+        payload = request.get_json(force=True)
+        if payload is None:
+            return None
+
+        longitude = payload["longitude"]
+        latitude = payload["latitude"]
+
+        # Update location
+        return "Longitude: {}, Latitude: {}".format(longitude, latitude)
+    elif request.method == 'GET':
+        # Get last location
+        return "GET"
+
+    return "No Request"
+
 
 connection = pymysql.connect(
-        host=config.host,
-        user=config.username,
-        password=config.password,
-        port=config.port,
-        database=config.database
-        )
+    host=config.host,
+    user=config.username,
+    password=config.password,
+    port=config.port,
+    database=config.database
+)
+
+
 def regDevice():
     global connection
     connection.ping(reconnect=True)
     cursor = pymysql.cursors.Cursor(connection)
-    
+
+
+app.run(debug=True)
