a7c5904cfd364e47ebcf6db0131c96794e96c382
Nathan Hare <nathanhare32@gmail.com>
2020-08-15 10:50:14 +1200

    CODEEEEEEEEEEEEEEEEEEEEE

--- covid19_swarm/lib/main.dart
+++ covid19_swarm/lib/main.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(CoVID19_Swarm());
+void main() => runApp(CoVID19Swarm());
 
-class CoVID19_Swarm extends StatelessWidget{
+class CoVID19Swarm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("CoVID Swarm")
-        ),
-        body: Center(child: Text("Hello world"),),
-      )
-    );
+        home: Scaffold(
+      appBar: AppBar(title: Text("CoVID Swarm")),
+      body: Center(
+        child: Text("Hello world"),
+      ),
+    ));
   }
-  
 }
