ce0d40105a590e834fcbb0547b929804dcd99303
Nathan Hare <nathanhare32@gmail.com>
2020-08-16 09:21:23 +1200

    UI Changes go brrr

--- CovidSwarm/lib/main.dart
+++ CovidSwarm/lib/main.dart
@@ -1,39 +1,46 @@
 import 'dart:convert';
 
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:CovidSwarm/get_location.dart';
 import 'package:background_fetch/background_fetch.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-Future<void> backgroundFetchHeadless(String taskID) {
+void updateGPS() {
   Pos location = Pos.err();
   getPosition().then((value) async {
     location = value;
     print("Location: ${location.toString()}");
-    
+
     var deviceID = await _getDeviceID();
     var client = http.Client();
     try {
-      var uriResponse = await client.post('http://swarm.qrl.nz/location/'+deviceID.toString(),
-        body: jsonEncode({"covid_status" : false, "latitude" : location.latitude.toString(), "longitude" : location.longitude.toString()})
-        );
-      print("Server status code: "+ uriResponse.statusCode.toString());
-      
-    } catch(e) {
-      print("Error on server post: "+e.toString());
-    }
-    finally {
+      var uriResponse = await client.post(
+          'http://swarm.qrl.nz/location/' + deviceID.toString(),
+          body: jsonEncode({
+            "covid_status": false,
+            "latitude": location.latitude.toString(),
+            "longitude": location.longitude.toString()
+          }));
+      print("Server status code: " + uriResponse.statusCode.toString());
+    } catch (e) {
+      print("Error on server post: " + e.toString());
+    } finally {
       client.close();
     }
   });
 }
 
+Future<void> backgroundFetchHeadless(String taskId) {
+  updateGPS();
+  BackgroundFetch.finish(taskId);
+}
+
 void main() {
   runApp(MyApp());
-  
+
   BackgroundFetch.registerHeadlessTask(backgroundFetchHeadless);
 }
 
@@ -55,15 +62,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -74,81 +72,81 @@ class _MyHomePageState extends State<MyHomePage> {
   Completer<GoogleMapController> _controller = Completer();
   Set<Heatmap> _heatmaps = {};
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  bool backgroundTaskEnabled = true;
 
   @override
   void initState() {
     super.initState();
     initPlatformState();
+    var fiveMinutes = Duration(minutes: 5);
+    Timer.periodic(fiveMinutes, (timer) {
+      setState(() {
+        _getServerGPS();
+        _refreshHeatmap();
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      key: _scaffoldKey,
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        heatmaps: _heatmaps,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.42796133580664, -122.085749655962),
-          zoom: 1.4746,
-        ),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: Row (
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          FloatingActionButton (
-            onPressed: _refreshHeatmap,
-            child: (
-              Text('Refresh Heatmap')
+    ;
+    _refreshHeatmap();
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            key: _scaffoldKey,
+            appBar: AppBar(
+              title: Text(widget.title),
+              bottom: TabBar(
+                tabs: [
+                  Tab(icon: Icon(Icons.map)),
+                  Tab(icon: Icon(Icons.settings))
+                ],
+              ),
             ),
-          ),
-          FloatingActionButton(
-            onPressed: _manuleUpdateGPS,
-            child: Text('Push GPS')),
-        ],
-      )
-
-    );
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                GoogleMap (
+                  mapType: MapType.hybrid,
+                  heatmaps: _heatmaps,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(37.42796133580664, -122.085749655962),
+                    zoom: 1.4746,
+                  ),
+                  onMapCreated: (GoogleMapController controller) {
+                    _controller.complete(controller);
+                  },
+                ),
+                Settings(this)
+              ],
+            )
+        ));
   }
 
   Future<void> initPlatformState() async {
-    BackgroundFetch.configure(BackgroundFetchConfig(
-      minimumFetchInterval: 30,
-      stopOnTerminate: false,
-      enableHeadless: true,
-      requiresBatteryNotLow: true,
-      requiresCharging: false,
-      requiresStorageNotLow: false,
-      requiresDeviceIdle: false,
-      requiredNetworkType: NetworkType.ANY
-    ), (String taskId) async {
+    BackgroundFetch.configure(
+            BackgroundFetchConfig(
+                minimumFetchInterval: 30,
+                stopOnTerminate: false,
+                enableHeadless: true,
+                requiresBatteryNotLow: true,
+                requiresCharging: false,
+                requiresStorageNotLow: false,
+                requiresDeviceIdle: false,
+                requiredNetworkType: NetworkType.ANY), (String taskId) async {
       print("[BackgroundFetch], received $taskId]");
-    }).then((int status) => {
-      print("[BackgroundFetch], configure success: $status")
-    }).catchError((e) => {
-      print("[BackgroundFetch], configure failure: $e")
-    });
+    })
+        .then((int status) =>
+            {print("[BackgroundFetch], configure success: $status")})
+        .catchError((e) => {print("[BackgroundFetch], configure failure: $e")});
 
     if (!mounted) return;
   }
 
   Future<void> _refreshHeatmap() async {
     var points = await _getPoints();
-    print("Server Points: "+ points.toString());
+    print("Server Points: " + points.toString());
     setState(() {
       _heatmaps.add(Heatmap(
           heatmapId: HeatmapId("people_tracking"),
@@ -171,10 +169,10 @@ class _MyHomePageState extends State<MyHomePage> {
     final decodedGPS = json.decode(serverJSON);
 
     for (var jsonGpsPoint in decodedGPS) {
-      points.add(_createWeightedLatLng(jsonGpsPoint["latitude"], jsonGpsPoint["longitude"], 1));
+      points.add(_createWeightedLatLng(
+          jsonGpsPoint["latitude"], jsonGpsPoint["longitude"], 1));
     }
 
-    
     return points;
   }
 
@@ -203,30 +201,62 @@ class _MyHomePageState extends State<MyHomePage> {
       return "[]";
     }
   }
-  
-
-  void _manuleUpdateGPS() {
-    backgroundFetchHeadless("ManuleShit");
-  }
-  
 }
 
 Future<int> _getDeviceID() async {
-    
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    if (prefs.containsKey("device_id")) {
-      	return prefs.getInt("device_id");
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (prefs.containsKey("device_id")) {
+    return prefs.getInt("device_id");
+  } else {
+    final response = await http.get('http://swarm.qrl.nz/device');
+    if (response.statusCode == 200) {
+      prefs.setInt("device_id", int.parse(response.body));
+      return int.parse(response.body);
     } else {
-      final response = await http.get('http://swarm.qrl.nz/device');
-      if (response.statusCode == 200) {
-        prefs.setInt("device_id", int.parse(response.body));
-        return int.parse(response.body);
-      } else {
-        // If the server did not return a 200 OK response,
-        // then throw an exception.
-        print("Failed to reg with server, satus:"+response.statusCode.toString());
-        return -1;
-      }
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      print(
+          "Failed to reg with server, status:" + response.statusCode.toString());
+      return -1;
     }
-    
   }
+}
+
+
+class Settings extends StatelessWidget {
+  _MyHomePageState homePage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text("Background upload is currently ${homePage.backgroundTaskEnabled ? "active" : "disabled"}"),
+        MaterialButton(
+          child: Text("Manual Update GPS"),
+          onPressed: updateGPS,
+        ),
+        MaterialButton(
+          child: Text("Toggle background task"),
+          onPressed: () {
+            homePage.setState(() {
+              homePage.backgroundTaskEnabled = !homePage.backgroundTaskEnabled;
+            });
+          },
+        ),
+        MaterialButton(
+          child: Text("Manual map update"),
+          onPressed: () {
+            homePage.setState(() {
+              homePage._getServerGPS();
+              homePage._refreshHeatmap();
+            });
+          }
+        )
+      ],
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+    );
+  }
+
+  Settings(this.homePage);
+}
