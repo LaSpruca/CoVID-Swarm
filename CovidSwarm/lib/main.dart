16bb0e78dd5f39d76d8bcb3afa17b04e92c7e68d
Nathan Hare <nathanhare32@gmail.com>
2020-08-15 18:08:10 +1200

    Setting up background tasks
    brrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr

--- CovidSwarm/lib/main.dart
+++ CovidSwarm/lib/main.dart
@@ -2,18 +2,22 @@ import 'dart:convert';
 import 'dart:ffi';
 
 import 'package:CovidSwarm/get_location.dart';
+import 'package:background_fetch/background_fetch.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 
+Future<void> backgroundFetchHeadless(String taskID) {
+  Pos location = Pos.err();
+  getPosition().then((value) => {location = value});
+  print("Location: ${location.toString()}");
+}
+
 void main() {
-  const fiveMinutes = const Duration(minutes: 5);
-  Timer.periodic(fiveMinutes, (t) {
-    print("Getting location");
-    getPosition().then((value) => print(value.toString()));
-  });
   runApp(MyApp());
+  
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadless);
 }
 
 class MyApp extends StatelessWidget {
@@ -23,23 +27,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Covid Swarm',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls wi ll be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Covid Swarm Map'),
-      //home: GoogleMapWidget(),
     );
   }
 }
@@ -62,54 +53,17 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-// class GoogleMapWidget extends StatefulWidget {
-//   @override
-//   State<GoogleMapWidget> createState() => GoogleMapWidgetState();
-// }
-
-// class GoogleMapWidgetState extends State<GoogleMapWidget> {
-//   Completer<GoogleMapController> _controller = Completer();
-
-//   static final CameraPosition _kGooglePlex = CameraPosition(
-//     target: LatLng(37.42796133580664, -122.085749655962),
-//     zoom: 14.4746,
-//   );
-
-//   static final CameraPosition _kLake = CameraPosition(
-//       bearing: 192.8334901395799,
-//       target: LatLng(37.43296265331129, -122.08832357078792),
-//       tilt: 59.440717697143555,
-//       zoom: 19.151926040649414);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return new Scaffold(
-//       body: GoogleMap(
-//         mapType: MapType.hybrid,
-//         initialCameraPosition: _kGooglePlex,
-//         onMapCreated: (GoogleMapController controller) {
-//           _controller.complete(controller);
-//         },
-//       ),
-//       floatingActionButton: FloatingActionButton.extended(
-//         onPressed: _goToTheLake,
-//         label: Text('To the lake!'),
-//         icon: Icon(Icons.directions_boat),
-//       ),
-//     );
-//   }
-
-//   Future<void> _goToTheLake() async {
-//     final GoogleMapController controller = await _controller.future;
-//     controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-//   }
-// }
-
 class _MyHomePageState extends State<MyHomePage> {
   Completer<GoogleMapController> _controller = Completer();
   final Set<Heatmap> _heatmaps = {};
   LatLng _heatmapLocation = LatLng(37.42796133580664, -122.085749655962);
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -143,6 +97,27 @@ class _MyHomePageState extends State<MyHomePage> {
         icon: Icon(Icons.refresh),
       ),
     );
+  }
+
+  Future<void> initPlatformState() async {
+    BackgroundFetch.configure(BackgroundFetchConfig(
+      minimumFetchInterval: 30,
+      stopOnTerminate: false,
+      enableHeadless: true,
+      requiresBatteryNotLow: true,
+      requiresCharging: false,
+      requiresStorageNotLow: false,
+      requiresDeviceIdle: false,
+      requiredNetworkType: NetworkType.ANY
+    ), (String taskId) async {
+      print("[BackgroundFetch], received $taskId]");
+    }).then((int status) => {
+      print("[BackgroundFetch], configure success: $status")
+    }).catchError((e) => {
+      print("[BackgroundFetch], configure failure: $e")
+    });
+
+    if (!mounted) return;
   }
 
   Future<void> _refreshHeatmap() async {
