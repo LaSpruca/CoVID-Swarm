73575f9fc043f972555efd81bc041d16b8a93fe3
Jasper <jasper@ngita.co.nz>
2020-08-15 23:13:32 +1200

    Backend change, get GPS returns latest from deivces

--- backend/main.py
+++ backend/main.py
@@ -91,7 +91,8 @@ def get_GPS():
     global connection
     connection.ping(reconnect=True)
     cursor = connection.cursor(cursor=pymysql.cursors.DictCursor)
-    query = "SELECT covid_status, latitude, longitude FROM locations"
+    query = "SELECT covid_status, latitude, longitude FROM locations GROUP BY device_id"
+
     try:
         cursor.execute(query)
         result = cursor.fetchall()
